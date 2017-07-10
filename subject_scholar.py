5422108baafa8dc0353b400668689679937b6bb5
Brian Coe <bwc126@gmail.com>
2017-07-09 21:40:23 -0400

    feat: Add subject_scholar class file

---
+++ subject_scholar.py
@@ -0,0 +1,3 @@
+# Subject Scholar is a class for finding hierarchical subject matter motifs in natural language. Finds common word roots and suffixes, thematic phrases, and specialized subject discourses.
+
+# Implementation: use CNN's and a custom information criterion to perform non-specific supervised learning. 
