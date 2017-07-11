76e22cf878ffe6b952c7809055af451dbb433fd1
Brian Coe <bwc126@gmail.com>
2017-07-10 23:31:13 -0400

    feat: Add function stubs for class

--- subject_scholar.py
+++ subject_scholar.py
@@ -1,3 +1,17 @@
 # Subject Scholar is a class for finding hierarchical subject matter motifs in natural language. Finds common word roots and suffixes, thematic phrases, and specialized subject discourses.
 
-# Implementation: use CNN's and a custom information criterion to perform non-specific supervised learning. 
+# Implementation: use CNN's and a custom information criterion to perform non-specific supervised learning. A model for extracting topics at various levels of linguistic hierarchy is selected and used to analyse bodies of text to determine their topical content. Model selection is a key process of the training phase, as not only do multiple models need to be trained, but compared on the basis of their statistical merits.
+
+class Subject_Scholar(object):
+    """ A topic analyzer that finds hierarchical subject matter motifs in natural language.
+
+    """
+    def __init__(self, criterion):
+        pass
+
+    def train(self, training_data):
+        pass
+
+    def analyse(self, data):
+        pass
+    
